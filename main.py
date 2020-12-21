0e3047301d027d74d2c9547a2e8d7586a13eefd4
Aditya Telange <21258296+adityatelange@users.noreply.github.com>
2020-12-21 14:23:50 +0530

    load images slug-wise and store them in an array

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # imports
 import pandas as pd
+from glob import glob
 
 # static declarations
 dataset_images_csv = "data/images.csv"
@@ -9,3 +10,22 @@ dataset_images = "data/images"
 profiles = pd.read_csv(dataset_images_csv)
 print("------------------------------------Original Profiles------------------------------------")
 print(profiles)
+
+print("------------------------------------Loading Images------------------------------------")
+
+slugs = profiles.slug.unique()
+# get all images
+all_jpgs = []
+for slug in slugs:
+    profile_images = glob(dataset_images + "/" + slug + "/*")
+    count = 0
+    # load only jpeg images
+    for img in profile_images:
+        if ".jpg" in img or ".jpeg" in img or "JPG" in img:
+            all_jpgs.append(img)
+            count += 1
+    print("{:0=3d} images from {}".format(count, slug))
+
+all_jpgs = sorted(all_jpgs)  # all images sorted
+print("-------------------")
+print("{:0=3d} images in Total".format(len(all_jpgs)))
